8a1c0a05db431dd4f6eccf9aa0b1fa27f1b55415
orion <louiseportela@edu.unifor.br>
2024-12-10 04:00:35 -0300

    transitions

---
+++ cenas/HUDs/transitions.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func change_scene(target: String) -> void:
+	$AnimationPlayer.play('fade-in')
+	await $AnimationPlayer.animation_finished
+	get_tree().change_scene_to_file(target)
+	$AnimationPlayer.play_backwards('fade-in')
